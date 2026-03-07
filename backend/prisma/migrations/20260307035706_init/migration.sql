2c7bc1238bc62ab4a8df2aae43451d6bdb06b31c
GodRyosuke <kusuprograming@gmail.com>
2026-03-07 13:01:52 +0900

    change db name to todo_db

---
+++ backend/prisma/migrations/20260307035706_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `session_data` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "session_data";
+
+-- DropTable
+DROP TABLE "users";
